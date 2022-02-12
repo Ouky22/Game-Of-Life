e3b32fe89c01835f785d0e4e1099f9e09a27dea9
Mike <menzelmike123@gmail.com>
2022-02-12 22:00:11 +0100

    add main methods for GameOfLiveManager
    provide methods for the ui to interact with the gameOfLive field

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 import ui.gui.GUI;
-import ui.tui.TUI;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length > 0 && args[0].equals("tui"))
-            new TUI();
-        else
-            new GUI();
+        new GUI();
     }
 }
--- src/domain/GameOfLiveManager.java
+++ src/domain/GameOfLiveManager.java
@@ -1,9 +1,45 @@
 package domain;
 
+
+import java.util.ArrayList;
+
 public class GameOfLiveManager {
     private GameOfLiveField gameOfLiveField;
+    private int speed = 1000;
 
     public GameOfLiveManager(int fieldHeight, int fieldWidth) {
         gameOfLiveField = new GameOfLiveField(fieldHeight, fieldWidth);
+        this.speed = speed;
+    }
+
+    public ArrayList<int[]> loadNextGeneration() {
+        return gameOfLiveField.loadNextGeneration();
+    }
+
+    /**
+     * set one cell in the gameOfLive field at the given row and column alive or not alive
+     *
+     * @param row    row of the cell
+     * @param column column of the cell
+     * @param alive  whether the cell should be alive or dead
+     */
+    public void setCellAt(int row, int column, boolean alive) {
+        gameOfLiveField.setCellAt(row, column, alive);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getFieldHeight() {
+        return gameOfLiveField.getHeight();
+    }
+
+    public int getFieldWidth() {
+        return gameOfLiveField.getWidth();
     }
 }
