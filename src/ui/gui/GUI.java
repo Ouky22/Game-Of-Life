691059a96369a7b00bd9abcb9dd7a23f340fc3c7
Mike <menzelmike123@gmail.com>
2022-02-17 21:04:32 +0100

    add method for toggling buttons in fieldPanel
    With toggleButtonsInFieldPanel it is possible for the resetPanel to
    toggle buttons at certain positions in the fieldPanel.
    This can be used if there are changes in the life status of cells outside of the
    timer interval e.g. when resetting the game to its first generation or
    killig all cells.

--- src/ui/gui/GUI.java
+++ src/ui/gui/GUI.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class GUI extends JFrame {
     private final int FIELD_WIDTH = 50;
@@ -20,16 +21,25 @@ public class GUI extends JFrame {
     public GUI() {
         this.setLayout(new BorderLayout());
 
-        // add ControlPanel, which is for adjusting settings for the gameOfLive
-        this.add(new ControlPanel(gameOfLiveManager), BorderLayout.NORTH);
-
         // add fieldPanel, which contains the gameOfLive field
         fieldPanel = new FieldPanel(gameOfLiveManager);
         this.add(fieldPanel, BorderLayout.CENTER);
 
+        // add ControlPanel, which is for adjusting settings for the gameOfLive
+        this.add(new ControlPanel(gameOfLiveManager, this), BorderLayout.NORTH);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(750, 750);
         this.setVisible(true);
+    }
+
+    /**
+     * toggle buttons at the given positions in the field panel
+     * @param coordinates of the buttons to be toggled
+     */
+    public void toggleButtonsInFieldPanel(ArrayList<int[]> coordinates) {
+        for (int[] coordinate : coordinates)
+            fieldPanel.toggleButton(coordinate);
     }
 
     class TimerListener implements ActionListener {
